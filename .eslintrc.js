dd0f813d30596441c358d5027f1e2b518eb56e23
Gabriel Carvajal <g.carvajal.pizarro@gmail.com>
2020-03-02 11:56:15 -0300

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:prettier/recommended'
   ],
   globals: {
     Atomics: 'readonly',
@@ -22,5 +23,6 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
   },
 };
