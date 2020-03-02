54534b6ac64b50d8ae8f45bcc9a9e650d79a4f8a
Gabriel Carvajal <g.carvajal.pizarro@gmail.com>
2020-03-02 16:36:55 -0300

    tested exercise about useState react hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+	const [count, setCount] = useState(0);
+	const changeFunction = () => {
+		setCount(prevCount => prevCount + 1);
+	}
+
 	return (
 		<div>
-			<h1>0</h1>
-			<button>Change!</button>
+			<h1>{count}</h1>
+			<button onClick={changeFunction}>Change!</button>
 		</div>
 	);
 }
